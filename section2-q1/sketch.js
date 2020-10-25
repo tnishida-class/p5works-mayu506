1bbe4c29999c1d200fa574cba4c51dda9f87d4bd
mayu506 <s6tm0506@gmail.com>
2020-10-25 15:01:11 +0900

    change section2-q1

--- section2-q1/sketch.js
+++ section2-q1/sketch.js
@@ -1,6 +1,13 @@
 // 小手調べ
 function setup(){
-  for(let i = 0; i < 10; i++){
-    // BLANK[1]
+  createCanvas(500,500);
+  for(let i = 0; i < 10; i = i + 1){
+    if(i < 5){
+      stroke(255,0,0)
+    }
+    else{
+      stroke(0,0,255)
+    }
+    ellipse(50, 50, 50-(i*5));
   }
 }
