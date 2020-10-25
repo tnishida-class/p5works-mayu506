f1ef7bf391152e4f7b389fbe7a02dedd0a5b4491
mayu506 <s6tm0506@gmail.com>
2020-10-25 15:23:45 +0900

    change section2-q2

--- section2-q2/sketch.js
+++ section2-q2/sketch.js
@@ -6,6 +6,25 @@ function setup() {
   for(let i = 0; i < 8; i++){
     for(let j = 0; j < 8; j++){
       // BLANK[1] (hint: rectのx座標は size * i, y座標はsize * j)
-    }
+      if(i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0){
+        fill(255);
+        rect(size*i, size*j, size, size);
+      }
+      else {
+        fill(192);
+        rect(size*i, size*j, size, size);
+
+        if(j < 3){
+        fill(255,0,0);
+      }
+      else if(j > 2 && j < 5){
+        fill(192);
+      }
+      else{
+        fill(0);
+      }
+        ellipse(size*i+size/2, size*j+size/2, 20);
+      }
   }
+}
 }
